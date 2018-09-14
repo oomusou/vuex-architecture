8100397060821422bbcdb638ce9ab5902edd70ba
oomusou <oomusou@gmail.com>
2018-09-14 09:32:48 +0800

    route done

---
+++ src/views/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Counter</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCounter',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Todo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+};
+</script>
+
+<style scoped>
+
+</style>
